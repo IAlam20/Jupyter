62a9051e28acd7e1e442988239d4ff957be80944
cryuaging <139259719+cryuaging@users.noreply.github.com>
2023-07-18 09:46:36 -0400

    Update Sample to Access STARS.py

--- Sample to Access STARS.py
+++ Sample to Access STARS.py
@@ -1,3 +1,15 @@
+-- Find out the program ID of Selfhelp Austin Senior Center
+-- Use the ID to restrict your data pulling
+-- Find out "Registration Table"
+-- Pull out top 100 clients records in Registration Table
+-- Just pull out any records related to the status of the client in the Registration Table
+-- pull out creator or modifier info from Registration Table
+-- Find out "Contact" table
+-- Pull out top 100 clients records in Contact Table
+-- Just pull out contact_id, name, gender, dob and creator or modifier info from Contact table
+-- combine records from registration and contact tables
+
+
 import pyodbc
 conn = pyodbc.connect('DRIVER={SQL Server};'
                       'SERVER=DFTASTARS\DFTASTARS;'
